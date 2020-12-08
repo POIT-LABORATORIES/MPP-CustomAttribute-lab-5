cc1cba1c415e971c42d75bca4502da1a1ce27350
Pluralment <Foolman.ru@yandex.ru>
2020-12-08 13:31:38 +0300

    update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
